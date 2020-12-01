f4910ecca36c073a41db3be1fec77c06c5ba541b
h-yes-oo <hyesoo5115@naver.com>
2020-12-01 11:00:00 +0900

    using javascript like typescript using jsDoc and ts-check

---
+++ why-ts/sample.js
@@ -0,0 +1,21 @@
+// function sum(a,b){
+//     return a+b;
+// }
+
+// sum()
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a first number
+ * @param {number} b second number
+ */
+
+ function sum(a,b){
+     return a+b;
+ }
+
+ sum(10,'20');
+
+ //이런 식으로 jsDoc과 ts-check를 활용하여 자바스크립트를 타입스크립트처럼 쓸 수도 있다
---
+++ why-ts/sample.ts
@@ -0,0 +1,7 @@
+function add(a:number,b:number) : number {
+    return a+b;
+}
+
+add(10,20);
+var result = add(10,20);
+result.toLocaleString(); // number에 적용가능한 API를 자동완성을 이용하여 모두 쉽게 사용할 수 있다는 장점
