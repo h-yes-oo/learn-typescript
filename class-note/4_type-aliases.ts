2cae9126ffa8241c2ba7e020c1994dc5efb55494
h-yes-oo <hyesoo5115@naver.com>
2020-12-04 11:00:00 +0900

    type aliases

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,23 @@
+interface Person {
+    name: string;
+    age: number;
+}
+
+type PersonType = {
+    name: string;
+    age: number;
+}
+
+var seho: Person ={
+    name: '세호',
+    age: 30
+}
+
+var hyesoo: PersonType = {
+    name: 'hyesoo',
+    age: 24
+}
+
+type MyString = string;
+var str: MyString = 'lalala';
+
